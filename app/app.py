e201e367ade45f8cbd1c3455e3946080688030c6
Sarvesh <sarvesh4232@gmail.com>
2021-01-23 22:56:17 +0530

    Distance Algorithm Finished

--- app/app.py
+++ app/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import mysql.connector
 import json
 
@@ -34,6 +34,59 @@ def home():
 def or_profile():
     return render_template('OrphanageProfile.html')
 
+@app.route('/getOrphanages')
+def getOrphanages():
+    lat = float(request.args.get('lat'))
+    lon = float(request.args.get('lon'))
+    print("Lat ")
+    print(type(lat))
+    print("lon: ")
+    print(type(lon))
+    connection = mysql.connector.connect(**config)
+    cursor = connection.cursor()
+    query = "SELECT *, SQRT(POW(69.1 * (latitude - %s), 2) + POW(69.1 * (%s - longitude) * COS(latitude / 57.3), 2)) AS distance FROM orphanage_addresses INNER JOIN orphanage_users ON orphanage_addresses.or_user_id = orphanage_users.or_user_id HAVING distance < 25 ORDER BY distance;"
+    # print("Query\n")
+    # print(query)
+    cursor.execute(query, (lat, lon,))
+    results = cursor.fetchall()
+    final = []
+    dic = {}
+    print("Results\n")
+    print(results)
+    for i in results:
+        for j in range(0, len(i)):
+            if(j == 2):
+                dic.update({"Address_Line1": i[j]})
+            elif(j == 3):
+                dic.update({"Address_Line2": i[j]})
+            elif(j == 4):
+                if(i[j] != ''):
+                    dic.update({"Address_line3": i[j]})
+            elif(j == 5):
+                dic.update({"Area": i[j]})
+            elif(j == 6):
+                dic.update({"City": i[j]})
+            elif(j == 7):
+                dic.update({"PIN_Code": i[j]})
+            elif(j == 8):
+                dic.update({"Latitude": i[j]})
+            elif(j == 9):
+                dic.update({"Longitude": i[j]})
+            elif(j == 11):
+                dic.update({"Orphanage_Name": i[j]})
+            elif(j == 12):
+                dic.update({"Phone_Number": i[j]})
+            elif(j == 13):
+                dic.update({"Caretaker_Name": i[j]})
+            elif(j == 14):
+                dic.update({"Email": i[j]})
+            elif(j == 15):
+                dic.update({"Distance":i[j]})
+        final.append(dic)
+
+    cursor.close()
+    connection.close()
+    return {"result":final}
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True, ssl_context='adhoc')
--- app/static/JS/getLocation.js
+++ app/static/JS/getLocation.js
@@ -1,12 +1,18 @@
     if (navigator.geolocation) {
       navigator.geolocation.getCurrentPosition(showPosition);
     } else { 
-      x.innerHTML = "Geolocation is not supported by this browser.";
+      alert("Geolocation is not supported by this browser.");
     }
   
   function showPosition(position) {
     console.log("Latitude: " + position.coords.latitude + "<br>Longitude: " + position.coords.longitude);
     $.ajax({url: "http://34.125.223.209/" + position.coords.latitude + "," + position.coords.longitude + ".json", 
+            success: function(result){
+                console.log(result);
+            }
+    });
+
+    $.ajax({url: "https://0.0.0.0:5000/getOrphanages?lat=" + position.coords.latitude + "&lon=" + position.coords.longitude, 
             success: function(result){
                 console.log(result);
             }
