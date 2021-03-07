fd8fa6a6775060c32e8740172ce598b4cab2338d
Sarvesh <sarvesh4232@gmail.com>
2021-03-07 17:47:00 +0530

    DB Export

--- db/orl.sql
+++ db/orl.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: db
--- Generation Time: Feb 26, 2021 at 08:20 AM
+-- Generation Time: Mar 07, 2021 at 12:15 PM
 -- Server version: 5.7.31
 -- PHP Version: 7.4.14
 
@@ -43,6 +43,43 @@ CREATE TABLE `addresses` (
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `admin_users`
+--
+
+CREATE TABLE `admin_users` (
+  `id` int(11) NOT NULL,
+  `first_name` varchar(50) NOT NULL,
+  `last_name` varchar(255) NOT NULL,
+  `phone_number` bigint(10) NOT NULL,
+  `email_id` varchar(50) NOT NULL,
+  `password` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `approvals`
+--
+
+CREATE TABLE `approvals` (
+  `id` int(11) NOT NULL,
+  `admin_id` int(11) DEFAULT NULL,
+  `or_user_id` int(11) NOT NULL,
+  `account_status` varchar(10) NOT NULL DEFAULT 'pending',
+  `timestamp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `approvals`
+--
+
+INSERT INTO `approvals` (`id`, `admin_id`, `or_user_id`, `account_status`, `timestamp`) VALUES
+(1, NULL, 11, 'approved', '2021-02-27 05:32:31'),
+(2, NULL, 12, 'pending', '2021-02-27 06:15:03');
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `orphanage_addresses`
 --
 
@@ -66,7 +103,8 @@ CREATE TABLE `orphanage_addresses` (
 INSERT INTO `orphanage_addresses` (`id`, `or_user_id`, `address_line1`, `address_line2`, `address_line3`, `area`, `city`, `pin_code`, `latitude`, `longitude`) VALUES
 (1, 1, '187, Kuttiappan 2nd Street', 'Medavakkam Tank Road', '', 'Kilpauk', 'Chennai', 600010, 13.087409019470215, 80.241477966308600),
 (2, 2, 'Newel Paradise', 'Dhanapal Street', '', 'West Mambalam', 'Chennai', 600033, 13.029458045959473, 80.220985412597660),
-(9, 10, '844 South Hague Freeway', 'Voluptatibus laborum', 'Dolorem incididunt o', 'Architecto delectus', 'Optio elit adipisi', 47, 13.020871162414550, 80.217315673828120);
+(10, 11, '36 North First Street', 'Explicabo Lorem per', 'Sint perspiciatis i', 'Minima voluptas exer', 'Ipsum sint facilis', 70, 13.020885467529297, 80.217323303222660),
+(11, 12, '870 Hague Parkway', 'Impedit sed vero es', 'Quos aliqua Dolore ', 'Quis exercitationem ', 'Facere dolore earum ', 9, 13.020875930786133, 80.217315673828120);
 
 -- --------------------------------------------------------
 
@@ -86,7 +124,8 @@ CREATE TABLE `orphanage_files` (
 --
 
 INSERT INTO `orphanage_files` (`id`, `or_user_id`, `registration`, `other`) VALUES
-(1, 10, 'orphanage_docs/Report_for_First_Review.pdf', 'orphanage_docs/Sarvesh_Resume_2_-_Page.pdf');
+(2, 11, 'Report_for_First_Review.pdf', 'orphanage_docs/Sarvesh_Resume_2_-_Page.pdf'),
+(3, 12, 'orphanage_docs/Report_for_First_Review.pdf', 'orphanage_docs/Sarvesh_Resume_2_-_Page.pdf');
 
 -- --------------------------------------------------------
 
@@ -102,17 +141,19 @@ CREATE TABLE `orphanage_users` (
   `caretaker_name` varchar(100) COLLATE utf8_unicode_ci NOT NULL,
   `no_of_children` int(11) NOT NULL,
   `about` text COLLATE utf8_unicode_ci NOT NULL,
-  `password` varchar(100) COLLATE utf8_unicode_ci NOT NULL
+  `password` varchar(100) COLLATE utf8_unicode_ci NOT NULL,
+  `account_status` tinyint(1) NOT NULL DEFAULT '0'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 --
 -- Dumping data for table `orphanage_users`
 --
 
-INSERT INTO `orphanage_users` (`or_user_id`, `orphanage_name`, `phone_number`, `email_id`, `caretaker_name`, `no_of_children`, `about`, `password`) VALUES
-(1, 'Karunai Illam', 9791201860, 'sarvesh4232@gmail.com', 'Sarvesh', 10, 'Love to care. Care to love.', 'sarsterror'),
-(2, 'Mini Vilas', 7358265069, 'madhumithaa1411@gmail.com', 'Madhumithaa', 20, 'Love ', 'mini'),
-(10, 'Avram Hill', 501, 'hysagivex@mailinator.com', 'Imani Mills', 93, 'Voluptates dignissim', 'Pa$$w0rd!');
+INSERT INTO `orphanage_users` (`or_user_id`, `orphanage_name`, `phone_number`, `email_id`, `caretaker_name`, `no_of_children`, `about`, `password`, `account_status`) VALUES
+(1, 'Karunai Illam', 9791201860, 'sarvesh4232@gmail.com', 'Sarvesh', 10, 'Love to care. Care to love.', 'sarsterror', 1),
+(2, 'Mini Vilas', 7358265069, 'madhumithaa1411@gmail.com', 'Madhumithaa', 20, 'Love ', 'mini', 1),
+(11, 'Cedric Bean', 960, 'gydydu@mailinator.com', 'Rae Long', 36, 'Veritatis in alias p', 'Pa$$w0rd!', 0),
+(12, 'Kareem Holt', 931, 'qevaciz@mailinator.com', 'Marny Dunlap', 81, 'Est accusantium cupi', 'Pa$$w0rd!', 0);
 
 -- --------------------------------------------------------
 
@@ -156,7 +197,8 @@ CREATE TABLE `transactions` (
 --
 
 INSERT INTO `transactions` (`id`, `user_id`, `or_user_id`, `item_name`, `quantity`, `timestamp`) VALUES
-(1, 1, 1, 'Rice', 10, '2021-02-07 06:05:45');
+(1, 1, 1, 'Rice', 10, '2021-02-07 06:05:45'),
+(13, 1, 1, 'Money', 200, '2021-03-06 11:29:47');
 
 -- --------------------------------------------------------
 
@@ -190,6 +232,20 @@ INSERT INTO `users` (`user_id`, `first_name`, `last_name`, `phone_number`, `emai
 ALTER TABLE `addresses`
   ADD PRIMARY KEY (`id`),
   ADD KEY `user_id_fk` (`user_id`);
+
+--
+-- Indexes for table `admin_users`
+--
+ALTER TABLE `admin_users`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `approvals`
+--
+ALTER TABLE `approvals`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `admin_id` (`admin_id`),
+  ADD KEY `or_user_id` (`or_user_id`);
 
 --
 -- Indexes for table `orphanage_addresses`
@@ -243,22 +299,34 @@ ALTER TABLE `addresses`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
+-- AUTO_INCREMENT for table `admin_users`
+--
+ALTER TABLE `admin_users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `approvals`
+--
+ALTER TABLE `approvals`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
 -- AUTO_INCREMENT for table `orphanage_addresses`
 --
 ALTER TABLE `orphanage_addresses`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
 
 --
 -- AUTO_INCREMENT for table `orphanage_files`
 --
 ALTER TABLE `orphanage_files`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT for table `orphanage_users`
 --
 ALTER TABLE `orphanage_users`
-  MODIFY `or_user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+  MODIFY `or_user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
 
 --
 -- AUTO_INCREMENT for table `requirements`
@@ -270,7 +338,7 @@ ALTER TABLE `requirements`
 -- AUTO_INCREMENT for table `transactions`
 --
 ALTER TABLE `transactions`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
 
 --
 -- AUTO_INCREMENT for table `users`
@@ -287,6 +355,13 @@ ALTER TABLE `users`
 --
 ALTER TABLE `addresses`
   ADD CONSTRAINT `user_id_fk` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`);
+
+--
+-- Constraints for table `approvals`
+--
+ALTER TABLE `approvals`
+  ADD CONSTRAINT `approvals_ibfk_1` FOREIGN KEY (`admin_id`) REFERENCES `admin_users` (`id`),
+  ADD CONSTRAINT `approvals_ibfk_2` FOREIGN KEY (`or_user_id`) REFERENCES `orphanage_users` (`or_user_id`);
 
 --
 -- Constraints for table `orphanage_addresses`
